2f97d48fada5642f0a3b1b78afd93b9002ac1885
fatima-masmas <87-f@gmx.de>
2023-02-15 16:57:30 +0100

    Routing ist geklappt :)

--- blogapp/src/App.jsx
+++ blogapp/src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter,  Route, Routes, Link} from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+
 
 const App = () => {
   return (
-    <div>
-      <h1>Blog</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link href="/"> Home </Link>
+          <Link href="/blog"> Blog </Link>
+        </nav>
+        <h1> dom 6 </h1>
+      
+        <Routes>
+            <Route path= "/" element={<Home />} />
+            <Route path= "/blog" element={<Blog />} />
+          </Routes>
+      
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
 
